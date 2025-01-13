19208aa6df61f9faf0bc696793eeba66c0e55d06
MICHELLEWAIRIMU04 <michelle@michelle>
2025-01-13 20:49:57 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def bark(self):
+        print("Wolf!")
+
     def sit(self):
         print("The dog is sitting.")
 
 fido = Dog()
+fido.bark()
 fido.sit()       
     
